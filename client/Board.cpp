77c5a9f2b51d532c33bc889552c213c2996e1e03
Suhaas Joshi <joshiesuhaas0@gmail.com>
2022-12-31 08:51:27 +0530

    client/board: correct board's square positions
    The square positions were incorrect, in the sense that m_board[0][0] was
    where m_board[7][0] was supposed to be, and m_board[0][7] was in
    m_board[0][0]'s position. This is corrected.
    
    The correction involved repositioning pawns and castles as well.

--- client/Board.cpp
+++ client/Board.cpp
@@ -10,9 +10,9 @@ Board::Board()
 
     int x = startPos.x, y = startPos.y;
     for (int i = 7; i >= 0; --i) {
-        for (int j = 7; j >= 0; --j) {
+        for (int j = 0; j < 8; ++j) {
             x += 50;
-            m_board[i][j] = new Square(i, j, (i + j) % 2 ? sf::Color(60, 179, 113) : sf::Color::White, sf::Vector2f(50, 50), sf::Vector2f(x, y));
+            m_board[j][i] = new Square(j, i, (i + j) % 2 ? sf::Color(60, 179, 113) : sf::Color::White, sf::Vector2f(50, 50), sf::Vector2f(x, y));
         }
         y += 50;
         x = startPos.x;
@@ -27,21 +27,21 @@ void Board::populateBoard()
 
     /* pawns */
     for (int i = 0; i < 8; ++i) {
-        p = new Pawn(Piece::White, m_board[1][i], m_board[1][i]->pos());
-        m_board[1][i]->setPiece(p);
-        p = new Pawn(Piece::Black, m_board[6][i], m_board[6][i]->pos());
-        m_board[6][i]->setPiece(p);
+        p = new Pawn(Piece::White, m_board[i][1], m_board[i][1]->pos());
+        m_board[i][1]->setPiece(p);
+        p = new Pawn(Piece::Black, m_board[i][6], m_board[i][6]->pos());
+        m_board[i][6]->setPiece(p);
     }
 
     /* castles */
     p = new Castle(Piece::Black, m_board[7][7], m_board[7][7]->pos());
     m_board[7][7]->setPiece(p);
-    p = new Castle(Piece::Black, m_board[7][0], m_board[7][0]->pos());
-    m_board[7][0]->setPiece(p);
+    p = new Castle(Piece::Black, m_board[0][7], m_board[0][7]->pos());
+    m_board[0][7]->setPiece(p);
     p = new Castle(Piece::White, m_board[0][0], m_board[0][0]->pos());
     m_board[0][0]->setPiece(p);
-    p = new Castle(Piece::White, m_board[0][7], m_board[0][7]->pos());
-    m_board[0][7]->setPiece(p);
+    p = new Castle(Piece::White, m_board[7][0], m_board[7][0]->pos());
+    m_board[7][0]->setPiece(p);
 }
 
 void Board::draw(sf::RenderWindow &window)
