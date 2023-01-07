78bf94af5dd1546fe898a38e87884adace8f665a
Suhaas Joshi <joshiesuhaas0@gmail.com>
2023-01-07 10:46:25 +0530

    client/pieces: implement notifying square of King's influence
    Like with Knight, all squares within King's range from its position should
    be notified, regardless of occupational status. Therefore isInBounds()
    is called for ensuring that the board position exists before we add the
    piece to the square on that position.
    
    Due to the manner in which the changes are made, it is redundant to
    check the bounds of a square inside isMoveValid(), therefore it now only
    checks if the square is empty or if it contains an opponent piece.

--- client/Pieces/King.cpp
+++ client/Pieces/King.cpp
@@ -1,4 +1,5 @@
 #include "King.hpp"
+#include "../Helper.hpp"
 
 King::King(Piece::Color color, Square *square) :
     Piece(square, color, Type::King, color == Color::White ? "Pieces/Resources/WhiteKing.png" : "Pieces/Resources/BlackKing.png", 10)
@@ -13,36 +14,59 @@ void King::generateMoves(Board &board)
     short file = square()->file();
     short rank = square()->rank();
 
-    if (isMoveValid(file - 1, rank - 1, board)) {
-        m_moves.push_back(board.m_board[file - 1][rank - 1]);
+    if (isInBounds(file - 1, rank - 1)) {
+        board.m_board[file - 1][rank - 1]->addPiece(this);
+        if (isMoveValid(file - 1, rank - 1, board)) {
+            m_moves.push_back(board.m_board[file - 1][rank - 1]);
+        }
     }
-    if (isMoveValid(file, rank - 1, board)) {
-        m_moves.push_back(board.m_board[file][rank - 1]);
+    if (isInBounds(file, rank - 1)) {
+        board.m_board[file][rank - 1]->addPiece(this);
+        if (isMoveValid(file, rank - 1, board)) {
+            m_moves.push_back(board.m_board[file][rank - 1]);
+        }
     }
-    if (isMoveValid(file + 1, rank - 1, board)) {
-        m_moves.push_back(board.m_board[file + 1][rank - 1]);
+    if (isInBounds(file + 1, rank - 1)) {
+        board.m_board[file + 1][rank - 1]->addPiece(this);
+        if (isMoveValid(file + 1, rank - 1, board)) {
+            m_moves.push_back(board.m_board[file + 1][rank - 1]);
+        }
     }
-    if (isMoveValid(file + 1, rank, board)) {
-        m_moves.push_back(board.m_board[file + 1][rank]);
+    if (isInBounds(file + 1, rank)) {
+        board.m_board[file + 1][rank]->addPiece(this);
+        if (isMoveValid(file + 1, rank, board)) {
+            m_moves.push_back(board.m_board[file + 1][rank]);
+        }
     }
-    if (isMoveValid(file + 1, rank + 1, board)) {
-        m_moves.push_back(board.m_board[file + 1][rank + 1]);
+    if (isInBounds(file + 1, rank + 1)) {
+        board.m_board[file + 1][rank + 1]->addPiece(this);
+        if (isMoveValid(file + 1, rank + 1, board)) {
+            m_moves.push_back(board.m_board[file + 1][rank + 1]);
+        }
     }
-    if (isMoveValid(file, rank + 1, board)) {
-        m_moves.push_back(board.m_board[file + 1][rank]);
+    if (isInBounds(file, rank + 1)) {
+        board.m_board[file][rank + 1]->addPiece(this);
+        if (isMoveValid(file, rank + 1, board)) {
+            m_moves.push_back(board.m_board[file][rank + 1]);
+        }
     }
-    if (isMoveValid(file - 1, rank + 1, board)) {
-        m_moves.push_back(board.m_board[file - 1][rank + 1]);
+    if (isInBounds(file - 1, rank + 1)) {
+        board.m_board[file - 1][rank + 1]->addPiece(this);
+        if (isMoveValid(file - 1, rank + 1, board)) {
+            m_moves.push_back(board.m_board[file - 1][rank + 1]);
+        }
     }
-    if (isMoveValid(file - 1, rank, board)) {
-        m_moves.push_back(board.m_board[file - 1][rank]);
+    if (isInBounds(file - 1, rank)) {
+        board.m_board[file - 1][rank]->addPiece(this);
+        if (isMoveValid(file - 1, rank, board)) {
+            m_moves.push_back(board.m_board[file - 1][rank]);
+        }
     }
 }
 
 bool King::isMoveValid(short file, short rank, Board &board) const
 {
-    return (file >= 0 && file < 8 && rank >= 0 && rank < 8)
-        && (board.m_board[file][rank]->isEmpty() || board.m_board[file][rank]->piece()->color() != color());
+    return (board.m_board[file][rank]->isEmpty() || board.m_board[file][rank]->piece()->color() != color());
 }
 
 void King::move(Square *square)
